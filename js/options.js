f6d5af068d9a321e922cfac39a334caad94db9f9
matthewbarta <matthewbartaa@gmail.com>
2020-05-09 00:13:52 -0400

    Jquery submission, needs regex on urls

--- js/options.js
+++ js/options.js
@@ -1,10 +1,36 @@
 var bkg = chrome.extension.getBackgroundPage();
 
 $(function() {
+    //Puts in the current url.
     chrome.storage.sync.get({current_url: ""}, function(url) {
-        $("#url").text('*' + url.current_url + '*');
-    })
-    $("#block-site").keyup(function() {
-        $("#url").text('*' + $('#block-site').val() + '*');
+        if(url.current_url != "") {
+            $('#block-site').val(url.current_url);
+            $("#url").text('*' + $('#block-site').val() + '*');
+        }
+        else {
+            $("#url").text('');
+        }
     });
+    //When modifying the url.
+    $("#block-site").keyup(function() {
+        if( $('#block-site').val() != '') {
+            $("#url").text('*' + $('#block-site').val() + '*');
+        }
+        else {
+            $("#url").text($('#block-site').val());
+        }
+    });
+    //On submission
+    $('#submit-button').click(function() {
+        bkg.console.log("Something was submitted.")
+        let val = $('#block-site').val();
+        if(val) {
+            $('#block-site').val('');
+            $("#url").text($('#block-site').val());
+
+        }
+        else {
+            bkg.console.log("Empty url.")
+        }
+    })
 });
