01a67acc0593f1bea98cf1b65ac36b0ef39c273d
matthewbarta <matthewbartaa@gmail.com>
2020-05-10 23:13:47 -0400

    Block sites whose time has run out

--- js/background.js
+++ js/background.js
@@ -1,6 +1,6 @@
+//Global variables
 let timeList = [];
 let blockList = [];
-//let timerActive = false;
 let activeIndex = -1;
 let timer;
 
@@ -14,52 +14,22 @@ chrome.runtime.onInstalled.addListener(function () {
 //Countdown method for when tabs are opened.
 const reduceTime = (index) => {
   timeList[index].time--;
-  console.log(timeList[index].time)
-  if(timeList[index].time == 0) {
-      stopCountdown(timer);
-  }
+  console.log(timeList[index].time);
+  //When time runs out.
+  if(timeList[index].time == 0) timeExceeded(index);
 };
+
+//When time runs out - stop the timer and push the url to blocklist.
+function timeExceeded(index) {
+  stopCountdown(timer);
+  alert(`You have reached your daily limit on ${timeList[index].url}`);
+  blockList.push(`*://${timeList[index].url}/*`);
+}
 
 //Function to stop the countdown.
 const stopCountdown = (timer) => {
-  clearInterval(timer)
-  alert(`You have ${timeList[activeIndex].time} seconds left on ${timeList[activeIndex].url}!`);
+  clearInterval(timer);
 };
-
-//Checks if active tab is on the timed list, if so time it.
-function timeActiveTab(url) {
-  for(let index = 0; index < timeList.length; index++) {
-    if(url.includes(timeList[index].url)) {
-      if(activeIndex == -1) {
-        //The timer should already be running.
-        timer = setInterval(reduceTime, 1000, index);
-        activeIndex = index;
-        return;
-      }
-      else if(activeIndex == index) {
-        //Same index as before, do nothing.
-        return;
-      }
-      //Stop an existing timer on a previous tab, start a new one on a current tab.
-      else {
-        stopCountdown(timer);
-        activeIndex = index;
-        //One second countdown timer on active tab.
-        timer = setInterval(reduceTime,1000, index);
-        return;
-      }
-    }
-  }
-  activeIndex = -1;
-}
-
-// TODO remember to update this later when it is time to use the blocklist, if needed.
-// chrome.storage.onChanged.addListener(function (changes) {
-//   if (changes.blockList != undefined) {
-//     blockList = changes.blockList.newValue;
-//   }
-// });
-
 
 //Whenever the timeList gets a new component added update it on this script.
 chrome.storage.onChanged.addListener(function (changes) {
@@ -67,22 +37,6 @@ chrome.storage.onChanged.addListener(function (changes) {
     timeList = changes.timeList.newValue;
   }
 });
-
-// TODO Remember to use this to block sites later when the block list is running.
-// chrome.webRequest.onBeforeRequest.addListener(
-//     function() {
-//         if(blockList.length > 0) {
-//           chrome.webRequest.onBeforeRequest.addListener(
-//             function() {
-//               return {cancel: true};
-//             }, {urls: blockList}, ["blocking"]
-//           );
-//         }
-//         else {
-//           return {cancel: false};
-//         }
-//     }, {urls: []}
-// );
 
 //When the tab changes, get the active tab and check if it's in the blocklist.
 chrome.tabs.onActivated.addListener(function(activeInfo) {
@@ -96,8 +50,47 @@ chrome.tabs.onUpdated.addListener(function(id, info, tab) {
   timeActiveTab(tab.url);
 });
 
+//Checks if active tab is on the timed list, if so time it.
+function timeActiveTab(url) {
+  for(let index = 0; index < timeList.length; index++) {
+    if(url.includes(timeList[index].url)) {
+      if(activeIndex == -1) {
+        //Start a timer.
+        timer = setInterval(reduceTime, 1000, index);
+        activeIndex = index;
+        return;
+      }
+      //Same URL as before, keep the timer running.
+      else if(activeIndex == index) {
+        return;
+      }
+      //Stop an existing timer on a previous tab, start a new one on a current tab.
+      else {
+        stopCountdown(timer);
+        activeIndex = index;
+        //One second countdown timer on active tab.
+        timer = setInterval(reduceTime,1000, index);
+        return;
+      }
+    }
+  }
+  //If the item isn't on the blocklist and there is an active timer, shut it off.
+  if(timer != undefined) stopCountdown(timer);
+  activeIndex = -1;
+}
 
-
-//TODO When blocking a URL, add the match schemes for webRequest urls.
-//url = '*://' + url + '/*';
-//blockList.push(url)
+//Blocks sites whose time has run out.
+chrome.webRequest.onBeforeRequest.addListener(
+    function() {
+        if(blockList.length > 0) {
+          chrome.webRequest.onBeforeRequest.addListener(
+            function() {
+              return {cancel: true};
+            }, {urls: blockList}, ["blocking"]
+          );
+        }
+        else {
+          return {cancel: false};
+        }
+    }, {urls: []}
+);
