b0f484d729d4d98c17c4b2f4f986edd057fc4069
matthewbarta <matthewbartaa@gmail.com>
2020-05-13 14:57:29 -0400

    Dynamically creates the HTML form for submitting the days/weeks, small CSS modifications

--- js/options.js
+++ js/options.js
@@ -1,6 +1,10 @@
 const bkg = chrome.extension.getBackgroundPage();
 
 $(function () {
+
+  //Creates the week elements.
+  createWeek();
+
   //Puts in the current url.
   chrome.storage.sync.get({ currentURL: "" }, function (url) {
     if (url.currentURL != "") {
@@ -66,3 +70,67 @@ chrome.storage.onChanged.addListener(function (changes) {
     $("#block-site").val(trimURL(changes.currentURL.newValue));
   }
 });
+
+//I put it into a function for if I want to make it so that users can individually select days, rather than see a whole form at once.
+function createWeek() {
+  const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+  for(let day = 0; day < weekdays.length; day++) {
+    createWeekday(weekdays[day]);
+  }
+}
+
+//Create individual days.
+function createWeekday(weekday) {
+  createParagraphElement('weekday-input', `${capitalize(weekday)}`, 'weekday-text');
+  createLabelElement('weekday-input', `${weekday}-hr`, 'Hours', 'hr-label');
+  createInputElement('weekday-input', 'number', `${weekday}-hr`, "0", 'input-time', `${weekday}-hr`, "0", "23");
+  createLabelElement('weekday-input', `${weekday}-min`, 'Minutes', 'min-label');
+  createInputElement('weekday-input', 'number', `${weekday}-min`, "0", 'input-time', `${weekday}-min`, "0", "59");
+  createLabelElement('weekday-input', `${weekday}-blocked`, 'Block All Day', 'checkbox-label');
+  createInputElement('weekday-input', 'checkbox', `${weekday}-blocked`, "", "weekday-checkbox", `${weekday}-blocked`);
+  createLabelElement('weekday-input', `${weekday}-unrestricted`, 'Unrestricted', 'checkbox-label');
+  createInputElement('weekday-input', 'checkbox', `${weekday}-unrestricted`, "", "weekday-checkbox", `${weekday}-unrestricted`);
+}
+
+//Creates a text paragraph element.
+function createParagraphElement(parentId="", innerHTML="", classTag="", idTag="") {
+  const parent = document.getElementById(parentId);
+  let paragraph = document.createElement('p');
+  paragraph.innerHTML = innerHTML;
+  //Optional parameters.
+  if(classTag != "") paragraph.className = classTag;
+  if(idTag != "") paragraph.id = idTag;
+  parent.appendChild(paragraph);
+}
+
+//Create the necessary label elements.
+function createLabelElement(parentId="", forTag="", innerHTML="", classTag="", idTag="") {
+  const parent = document.getElementById(parentId);
+  let label = document.createElement('label');
+  label.htmlFor = forTag;
+  label.innerHTML = innerHTML;
+  //Optional parameters.
+  if(classTag != "") label.className = classTag;
+  if(idTag != "") label.id = idTag;
+  parent.appendChild(label);
+}
+
+//Create the necessary input elements.
+function createInputElement(parentId="", type="", name="", value="", classTag="", idTag="", min="", max="") {
+  const parent = document.getElementById(parentId);
+  let input = document.createElement('input');
+  input.type = type;
+  input.name = name;
+  //Optional parameters.
+  if(value != "") input.value = value;
+  if(classTag != "") input.className = classTag;
+  if(idTag != "") input.id = idTag;
+  if(min != "") input.min = min;
+  if(max != "") input.max = max;
+  parent.appendChild(input);
+}
+
+//Capitalize the first letter.
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
