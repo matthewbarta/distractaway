f78a4e9e80f6333968f377d18bc9b34983e34da1
matthewbarta <matthewbartaa@gmail.com>
2020-07-01 00:52:53 -0400

    All site list items inline

--- js/options.js
+++ js/options.js
@@ -11,6 +11,17 @@ const DIVS = ["add-site", "site-list", "general"];
 const zeroRegex = /00+/;
 let siteList = [];
 let pin = "";
+const chevronPath = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-double-up" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M7.646 2.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 3.707 2.354 9.354a.5.5 0 1 1-.708-.708l6-6z"/>
+<path fill-rule="evenodd" d="M7.646 6.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 7.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+</svg>`;
+const removePath = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-x-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.146-3.146a.5.5 0 0 0-.708-.708L8 7.293 4.854 4.146a.5.5 0 1 0-.708.708L7.293 8l-3.147 3.146a.5.5 0 0 0 .708.708L8 8.707l3.146 3.147a.5.5 0 0 0 .708-.708L8.707 8l3.147-3.146z"/>
+</svg>`;
+const trashPath = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>`;
 
 // ? Stats tab time spent on each site.
 
@@ -281,7 +292,7 @@ function getTimesByWeekday(id = "") {
 
 //Creates the dropdown menu for selecting weekdays.
 function createWeekDropdown(parentElement, id = "") {
-  createDiv(parentElement, "dropdown", `dropdown-${id}`);
+  createDiv(parentElement, "dropdown dropdown-button", `dropdown-${id}`);
   dropdownProperties = [
     { property: "data-toggle", value: "dropdown" },
     { property: "aria-haspopup", value: "true" },
@@ -290,7 +301,7 @@ function createWeekDropdown(parentElement, id = "") {
   createButtonElement(
     `dropdown-${id}`,
     "Modify Day",
-    "btn btn-secondary btn-lg dropdown-toggle",
+    "btn btn-secondary dropdown-toggle",
     `add-site-button-${id}`,
     dropdownProperties
   );
@@ -520,27 +531,27 @@ function createSiteList(siteList) {
 //Creates an individual site.
 function createSite(site, id = "") {
   createDiv("sites", "site-div", `site-div-${id}`);
-  createHeaderElement(`site-div-${id}`, site.url, 6, "site-names");
   createButtonElement(
     `site-div-${id}`,
-    "Remove",
-    "remove-buttons",
+    trashPath,
+    "remove-button btn btn-secondary",
     `site-remove-${id}`
   );
+  createHeaderElement(`site-div-${id}`, site.url, 6, "site-name");
   createWeekDropdown(`site-div-${id}`, id);
+  //Adds the cancel button.
+  createButtonElement(
+    `dropdown-${id}`,
+    chevronPath,
+    "btn btn-secondary",
+    `cancel-edit-${id}`
+  );
   //Adds submit button.
   createButtonElement(
     `dropdown-${id}`,
     "Submit Changes",
-    "btn btn-secondary btn-lg",
+    "btn btn-secondary",
     `submit-edit-${id}`
-  );
-  //Adds the cancel button.
-  createButtonElement(
-    `dropdown-${id}`,
-    "Cancel Changes",
-    "btn btn-secondary btn-lg",
-    `cancel-edit-${id}`
   );
   //Hides the submit/cancel button by default.
   $(`#submit-edit-${id}`).hide();
